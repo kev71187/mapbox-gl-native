c9e6848aadc9c1b31404321862aadafb35522d5a
Kevin Woods <kevin@kevinwoods.net>
2019-07-15 14:22:57 -0400

    install

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-cd mapbox-gl-native
 git submodule init
 git submodule update
 # # This takes like 20 mins
 time make iframework FORMAT=static BUILDTYPE=Release
-<<<<<<< HEAD
-=======
-
->>>>>>> 75fe45da41778428877d7ea2bf5bb422b6ea002c
