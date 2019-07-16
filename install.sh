d2fb7ca048f7f2dfe7b2a7a1897dbd5d796bef6e
Kevin Woods <kevin@kevinwoods.net>
2019-07-16 11:59:39 -0400

    Add install script

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+nvm use 7
 git submodule init
 git submodule update
-# # This takes like 20 mins
-time make iframework FORMAT=static BUILDTYPE=Release
+time make iframework BUILDTYPE=Release
+
+echo "New framework built to: ./build/ios/pkg/dynamic/Mapbox.framework"
+echo "Coping the framework to s3"
+
+# grab the latest version and increment it
+directory=s3://indigo-mapbox-distribution/framework/
+lastVersion=$(aws s3 ls "$directory" | grep -o indigo-.* | sed 's/[^0-9]//g' | sort -n | tail -1)
+nextVersion=$((lastVersion + 1))
+version="ios-v5.2.0-alpha.3-indigo-$nextVersion"
+echo "Next version", $version
+aws s3 sync ./build/ios/pkg/dynamic "$directory$version/"
+echo "Package located at: "
+echo "https://indigo-mapbox-distribution.s3.amazonaws.com/framework/$version/Mapbox.framework"
+echo "Change the s3 url located https://github.com/indigo-ag/react-native-mapbox-gl/blob/develop/scripts/download-mapbox-gl-native-ios.sh"
+
